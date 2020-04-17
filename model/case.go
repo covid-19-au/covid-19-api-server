e16203d8f1ec8ccdc4215bc7b290242b70cb80e5
Robin Liu <opensource@greenvine.dev>
2020-04-17 10:09:25 +0800

    feat(case): add GetCases implementation

--- model/case.go
+++ model/case.go
@@ -1,94 +1,95 @@
 package model
 
 import (
-    "errors"
     "fmt"
     "github.com/Kamva/mgm"
     pb "github.com/covid-19-au/covid-19-api-server/protogen/go"
 )
 
 type Case struct {
-	mgm.DefaultModel `bson:",inline"` // defines _id, created_at and updated_at
+    mgm.DefaultModel `bson:",inline"` // defines _id, created_at and updated_at
 
-	PatientID       string       `json:"patient_id" bson:"patient_id"`
-	Location        CaseLocation `json:"location" bson:"location"`
-	CaseState       string       `json:"case_state" bson:"case_state"`
-	InfectionSource string       `json:"inf_src" bson:"inf_src"`
+    PatientID       string       `json:"patient_id" bson:"patient_id"`
+    Location        CaseLocation `json:"location" bson:"location"`
+    CaseState       string       `json:"case_state" bson:"case_state"`
+    InfectionSource string       `json:"inf_src" bson:"inf_src"`
+    ReportedTime    int64        `json:"report_ts" bson:"report_ts"`
 }
 
 type CaseLocation struct {
-	NamedLocation *NamedCaseLocation `json:"named" bson:"named"`
-	GPSLocation   *GPSCaseLocation   `json:"gps" bson:"gps"`
-}
-
-type NamedCaseLocation struct {
-	Country    string `json:"country" bson:"country"`
-	Region     string `json:"region" bson:"region"`
-	City       string `json:"city" bson:"city"`
-	PostalCode string `json:"post_code" bson:"post_code"`
-}
-
-type GPSCaseLocation struct {
-	Latitude     float64 `json:"lat" bson:"lat"`
-	Longitude    float64 `json:"lng" bson:"lng"`
-	PrecisionRad uint64  `json:"prec_rad" bson:"prec_rad"`
+    Country    string `json:"country" bson:"country"`
+    Region     string `json:"region" bson:"region"`
+    City       string `json:"city" bson:"city"`
+    PostalCode string `json:"post_code" bson:"post_code"`
+    Latitude     float64 `json:"lat" bson:"lat"`
+    Longitude    float64 `json:"lng" bson:"lng"`
+    PrecisionRad uint64  `json:"prec_rad" bson:"prec_rad"`
 }
 
 func NewCase(req *pb.NewCaseDetail) (*Case, error) {
-	if req == nil {
-		return nil, fmt.Errorf("empty or invalid case details")
-	}
-
-	caseDetail := *req
-	var namedCaseLocation *NamedCaseLocation
-	var gpsCaseLocation *GPSCaseLocation
-
-	if loc := caseDetail.GetNamedLocation(); loc != nil {
-		namedCaseLocation = &NamedCaseLocation{
-			Country:    loc.GetCountry(),
-			Region:     loc.GetRegion(),
-			City:       loc.GetCity(),
-			PostalCode: loc.GetPostalCode(),
-		}
-	}
-
-	if loc := caseDetail.GetGpsLocation(); loc != nil {
-		gpsCaseLocation = &GPSCaseLocation{
-			Latitude:     loc.GetGeoLat(),
-			Longitude:    loc.GetGeoLng(),
-			PrecisionRad: loc.GetPrecisionRad(),
-		}
-	}
-
-	if namedCaseLocation == nil && gpsCaseLocation == nil {
-		return nil, fmt.Errorf("specify either named or GPS case location")
-	}
-
-	return &Case{
-		PatientID: caseDetail.GetPatientId(),
-		Location: CaseLocation{
-			NamedLocation: namedCaseLocation,
-			GPSLocation:   gpsCaseLocation,
-		},
-		CaseState:       caseDetail.GetState().String(),
-		InfectionSource: caseDetail.GetInfectSrc().String(),
-	}, nil
-}
-
-func (m *Case) Saving() error {
-	if err := m.DefaultModel.Saving(); err != nil {
-	   return err
-	}
-
-	// validate case state
-	if m.CaseState == "_UNASSIGNED_CASE_STATE" {
-	   return errors.New("invalid case state")
-	}
-
-	// validate infection source
-    if m.InfectionSource == "_UNASSIGNED_INF_SRC" {
-        return errors.New("invalid case infection source")
+    if req == nil {
+        return nil, fmt.Errorf("empty or invalid case details")
     }
 
-	return nil
+    caseDetail := *req
+
+    // run proto validator
+    if err := caseDetail.Validate(); err != nil {
+        return nil, fmt.Errorf("validation error: %v", err)
+    }
+    // additional checks for enums
+    if val, ok := pb.CaseState_value[caseDetail.GetState().String()]; !ok || val < 1 {
+        return nil, fmt.Errorf("validation error: invalid case state")
+    }
+    if val, ok := pb.InfectionSource_value[caseDetail.GetInfectSrc().String()]; !ok || val < 1 {
+        return nil, fmt.Errorf("validation error: invalid case infection source")
+    }
+
+    caseLocation := caseDetail.GetLocation()
+
+    return &Case{
+        PatientID: caseDetail.GetPatientId(),
+        Location: CaseLocation{
+            Country:      caseLocation.GetCountry(),
+            Region:       caseLocation.GetRegion(),
+            City:         caseLocation.GetCity(),
+            PostalCode:   caseLocation.GetPostalCode(),
+            Latitude:     caseLocation.GetGeoLat(),
+            Longitude:    caseLocation.GetGeoLng(),
+            PrecisionRad: caseLocation.GetGeoPrecisionRad(),
+        },
+        CaseState:       caseDetail.GetState().String(),
+        InfectionSource: caseDetail.GetInfectSrc().String(),
+        ReportedTime:    caseDetail.GetReportedTime(),
+    }, nil
+}
+
+func (m *Case) GetCaseState() pb.CaseState {
+    if val, ok := pb.CaseState_value[m.CaseState]; !ok {
+        return 0
+    } else {
+        return pb.CaseState(val)
+    }
+}
+
+func (m *Case) GetInfectionSource() pb.InfectionSource {
+    if val, ok := pb.InfectionSource_value[m.InfectionSource]; !ok {
+        return 0
+    } else {
+        return pb.InfectionSource(val)
+    }
+}
+
+func (m *Case) GetLocation() *pb.Location {
+    loc := m.Location
+
+    return &pb.Location{
+        Country:         loc.Country,
+        Region:          loc.Region,
+        City:            loc.City,
+        PostalCode:      loc.PostalCode,
+        GeoLat:          loc.Latitude,
+        GeoLng:          loc.Longitude,
+        GeoPrecisionRad: loc.PrecisionRad,
+    }
 }
--- service/case.go
+++ service/case.go
@@ -3,52 +3,117 @@ package service
 import (
     "fmt"
     "github.com/Kamva/mgm"
+    "github.com/Kamva/mgm/operator"
     m "github.com/covid-19-au/covid-19-api-server/model"
     pb "github.com/covid-19-au/covid-19-api-server/protogen/go"
     u "github.com/covid-19-au/covid-19-api-server/util"
     "github.com/golang/protobuf/ptypes/wrappers"
+    "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "golang.org/x/net/context"
     "google.golang.org/grpc/grpclog"
 )
 
-func (s PublicServer) GetCases(context.Context, *pb.GetCasesRequest) (*pb.GetCasesResponse, error) {
-	panic("implement me")
+func (s PublicServer) GetCases(ctx context.Context, req *pb.GetCasesRequest) (*pb.GetCasesResponse, error) {
+    if req == nil {
+        return nil, fmt.Errorf("empty or invalid request")
+    }
+
+    var results []m.Case
+    caseStates := req.GetStates()
+    caseStartTime := req.GetStartTime()
+    caseEndTime := req.GetEndTime()
+    query := buildQueryFromRequest(req.GetLocation(), &caseStates, &caseStartTime, &caseEndTime) // build Mongo query
+
+    if err := mgm.Coll(&m.Case{}).SimpleFind(&results, query); err != nil { // find matched cases
+        grpclog.Errorf("error occurred when querying case: %v\n", err)
+        return nil, fmt.Errorf("internal service error")
+    } else {
+        var cases []*pb.ExistingCaseDetail
+        for _, c := range results { // for each case, convert it to protobuf struct
+            cases = append(cases, &pb.ExistingCaseDetail{
+                CaseId:       c.ID.Hex(),
+                PatientId:    c.PatientID,
+                ReportedTime: c.ReportedTime,
+                State:        c.GetCaseState(),
+                InfectSrc:    c.GetInfectionSource(),
+                Location:     c.GetLocation(),
+            })
+        }
+
+        return &pb.GetCasesResponse{
+            Cases: cases,
+            // TODO (Robin): implement pagination
+            Pagination: &pb.Pagination{
+                TotalResults: uint32(len(results)),
+            },
+        }, nil
+    }
+}
+
+func (s PublicServer) GetCaseStats(ctx context.Context, req *pb.GetCaseStatsRequest) (*pb.GetCaseStatsResponse, error) {
+    return nil, fmt.Errorf("not implemented")
 }
 
 func (s AdminServer) AddCases(ctx context.Context, req *pb.AddCasesRequest) (*wrappers.BoolValue, error) {
-	if req == nil || len(req.Cases) < 1 {
-		return u.WrapBool(false), fmt.Errorf("empty or invalid request")
-	}
+    if req == nil || len(req.Cases) < 1 {
+        return u.WrapBool(false), fmt.Errorf("empty or invalid request")
+    }
 
-	var caseModels []*m.Case
-	for _, c := range req.Cases { // loop each case and build model
-		if newModel, err := m.NewCase(c); err != nil {
-			grpclog.Warningf("error occurred when adding case: %v\n", err)
-			return u.WrapBool(false), fmt.Errorf("empty or invalid case details")
-		} else {
-			caseModels = append(caseModels, newModel) // append to model list
-		}
-	}
+    var caseModels []*m.Case
+    for _, c := range req.Cases { // loop each case and build model
+        if newModel, err := m.NewCase(c); err != nil {
+            grpclog.Warningf("error occurred when adding case: %v\n", err)
+            return u.WrapBool(false), fmt.Errorf("invalid case details: %v", err)
+        } else {
+            caseModels = append(caseModels, newModel) // append to model list
+        }
+    }
 
-	// run transaction to insert all generated models
-	err := mgm.Transaction(func(session mongo.Session, sc mongo.SessionContext) error {
-		for _, c := range caseModels {
-			if err := mgm.Coll(c).CreateWithCtx(sc, c); err != nil { // perform insertion
-				return err
-			}
-		}
+    // run transaction to insert all generated models
+    err := mgm.Transaction(func(session mongo.Session, sc mongo.SessionContext) error {
+        for _, c := range caseModels {
+            if err := mgm.Coll(c).CreateWithCtx(sc, c); err != nil { // perform insertion
+                return err
+            }
+        }
 
-		return session.CommitTransaction(sc) // commit transaction if successful
-	})
+        return session.CommitTransaction(sc) // commit transaction if successful
+    })
 
-	return u.WrapBool(err == nil), err
+    return u.WrapBool(err == nil), err
 }
 
 func (s AdminServer) PutCases(context.Context, *pb.PutCasesRequest) (*wrappers.BoolValue, error) {
-	panic("implement me")
+    return nil, fmt.Errorf("not implemented")
 }
 
 func (s AdminServer) DelCases(context.Context, *pb.DelCasesRequest) (*wrappers.BoolValue, error) {
-	panic("implement me")
+    return nil, fmt.Errorf("not implemented")
+}
+
+func buildQueryFromRequest(loc *pb.Location, states *[]pb.CaseState, startTime, endTime *int64) bson.M {
+    query := bson.M{}
+
+    // TODO (Robin): add case locations
+    //if loc != nil {
+    //    query["location"] = ""
+    //}
+
+    // case states to be included
+    if states != nil {
+        query["case_state"] = bson.D{{operator.In, states}}
+    }
+
+    // report starting timestamp
+    if startTime != nil && *startTime > 0 {
+       query["report_ts"] = bson.M{operator.Gte: *startTime}
+    }
+
+    // report ending timestamp: must be greater than starting time iff starting time is specified
+    if endTime != nil && (startTime == nil || *endTime >= *startTime) {
+        query["report_ts"] = bson.M{operator.Lte: *endTime}
+    }
+
+    return query
 }
